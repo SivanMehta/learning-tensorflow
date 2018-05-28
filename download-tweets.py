ccaa1ed29d3acdb8f10a97e0aed21f3aa1d10c92
Sivan Mehta <Sivanmehta2@gmail.com>
2018-05-28 12:31:37 -0700

    define credentials and list tweets

--- download-tweets.py
+++ download-tweets.py
@@ -2,8 +2,15 @@ from __future__ import absolute_import, print_function
 import twitter
 import json
 
+# get the credentials
 credentials = {}
 with open('secrets.json') as json_data:
   credentials = json.load(json_data)
 
-print(credentials)
+api = twitter.Api(consumer_key=credentials['CONSUMER_KEY'],
+                  consumer_secret=credentials['CONSUMER_SECRET'],
+                  access_token_key=credentials['ACCESS_TOKEN'],
+                  access_token_secret=credentials['ACCESS_TOKEN_SECRET'])
+
+# get the top 10 trends
+trends = api.GetTrendsWoeid(woeid=23424977, exclude=None)[:10]
