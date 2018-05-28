e5688ede090001b239df6bee73fa27a0caed719a
Sivan Mehta <Sivanmehta2@gmail.com>
2018-05-28 16:55:52 -0700

    get tweets for all trends

--- download-tweets.py
+++ download-tweets.py
@@ -20,17 +20,11 @@ api = twitter.Api(
   access_token_secret=credentials['ACCESS_TOKEN_SECRET']
 )
 
-# get the top 10 trends
-trends = api.GetTrendsWoeid(woeid=23424977, exclude=None)[:10]
-
-trend = trends[0]
-now = datetime.date.today()
-query = "q={}&result_type=recent&since={}-{}-{}&count=1000".format(
-  urllib.parse.quote_plus(trend.name),
-  now.year,
-  now.month,
-  now.day
-)
+def removeLinks(tweet):
+  noLink = re.sub('https?://.*$', "", tweet.text)
+  noLink = re.sub('\n', "", noLink)
+  tweet.text = noLink
+  return tweet
 
 def removeLinks(tweet):
   noLink = re.sub('https?://.*$', "", tweet.text)
@@ -38,25 +32,37 @@ def removeLinks(tweet):
   tweet.text = noLink
   return tweet
 
-tweets = api.GetSearch(raw_query=query)
-tweets = map(removeLinks, tweets)
-
 def getMetaData(tweet):
   return (tweet.text, tweet.favorite_count)
 
-with concurrent.futures.ThreadPoolExecutor(max_workers = 5) as executor:
-  future_to_tweet = { executor.submit(getMetaData, t): t for t in tweets }
-  enriched = []
+def getTweets(trend):
+  now = datetime.date.today()
+  query = "q={}&result_type=recent&since={}-{}-{}&count=1000".format(
+    urllib.parse.quote_plus(trend.name),
+    now.year,
+    now.month,
+    now.day
+  )
 
-  for future in concurrent.futures.as_completed(future_to_tweet):
-    tweet = future_to_tweet[future]
-    try:
-        data = future.result()
-    except Exception as exc:
-        print('%r generated an exception: %s' % (tweet, exc))
-    else:
-        enriched.append(data)
+  tweets = api.GetSearch(raw_query=query)
+  tweets = map(removeLinks, tweets)
 
-  print("text,likes")
-  for tweet in enriched:
-    print("%s,%d" % (tweet[0], tweet[1]))
+  with concurrent.futures.ThreadPoolExecutor(max_workers = 5) as executor:
+    future_to_tweet = { executor.submit(getMetaData, t): t for t in tweets }
+    enriched = []
+
+    for future in concurrent.futures.as_completed(future_to_tweet):
+      tweet = future_to_tweet[future]
+      try:
+          data = future.result()
+      except Exception as exc:
+          print('%r generated an exception: %s' % (tweet, exc))
+      else:
+          enriched.append(data)
+
+    for tweet in enriched:
+      print("%s,%d,%s" % (tweet[0], tweet[1], trend.name))
+
+print("text,likes,trend")
+for trend in api.GetTrendsWoeid(woeid=23424977, exclude=None)[:10]:
+  getTweets(trend)
